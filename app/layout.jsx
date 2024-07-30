68506d940e9402bb3bd7bba44a096f122a536381
Rakshit Pandey <rakshit.cosmotechai@gmail.com>
2024-07-30 11:21:16 +0530

    Home

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,27 @@
 import '@styles/globals.css';
-
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 export const metadata = {
-    title:"Promptopia",
-    description:"Discover & Share AI Prompts"
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts"
 }
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+const RootLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
 }
 
-export default layout
+export default RootLayout
